a95c1a023cfaee3dba6b0c76ec589678f3227589
copyandbuild <devlarrox@gmail.com>
2025-08-29 22:21:42 +0200

    added PlayerUtil#isConsole Changed version to <VERSION>-SNAPSHOT added SNAPSHOT Distribution

--- src/main/java/dev/larrox/PlayerUtil.java
+++ src/main/java/dev/larrox/PlayerUtil.java
@@ -1,11 +1,17 @@
 package dev.larrox;
 
+import org.bukkit.block.CommandBlock;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 
 public class PlayerUtil {
 
     public static boolean isPlayer(CommandSender sender) {
         return sender instanceof Player;
+    }
+
+    public static boolean isConsole(CommandSender sender) {
+        return sender instanceof ConsoleCommandSender;
     }
 }
