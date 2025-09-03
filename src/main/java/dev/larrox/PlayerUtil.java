a2f6288da598eeefe053b20d70e4d6b320a38077
copyandbuild <devlarrox@gmail.com>
2025-09-04 01:38:04 +0200

    added some stuff in PlayerUtil.java

--- src/main/java/dev/larrox/PlayerUtil.java
+++ src/main/java/dev/larrox/PlayerUtil.java
@@ -1,8 +1,10 @@
 package dev.larrox;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
+import java.util.UUID;
 
 public class PlayerUtil {
 
@@ -12,5 +14,41 @@ public class PlayerUtil {
 
     public static boolean isConsole(CommandSender sender) {
         return sender instanceof ConsoleCommandSender;
+    }
+
+    public static Player getPlayer(CommandSender sender) {
+        return isPlayer(sender) ? (Player) sender : null;
+    }
+
+    public static UUID getUUID(Player player) {
+        return player != null ? player.getUniqueId() : null;
+    }
+
+    public static Player getPlayerByName(String name) {
+        return Bukkit.getPlayerExact(name);
+    }
+
+    public static Player getPlayerByUUID(UUID uuid) {
+        return Bukkit.getPlayer(uuid);
+    }
+
+    public static boolean isOnline(String name) {
+        return getPlayerByName(name) != null;
+    }
+
+    public static boolean isOnline(UUID uuid) {
+        return getPlayerByUUID(uuid) != null;
+    }
+
+    public static void sendMessage(CommandSender sender, String message) {
+        if (sender != null && message != null) {
+           ChatUtil.send(sender, message);
+        }
+    }
+
+    public static void kick(Player player, String reason) {
+        if (player != null) {
+            player.kickPlayer(reason != null ? reason : "You have been kicked!");
+        }
     }
 }
